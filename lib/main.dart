54cf82084284a26bf64356c373bc9eba890d5043
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2025-03-29 00:31:00 -0400

    Update .gitignore, Podfile.lock, and main.dart; enable GPU validation in Runner scheme; register AppSettingsPlugin in GeneratedPluginRegistrant.swift

--- lib/main.dart
+++ lib/main.dart
@@ -111,7 +111,7 @@ class MainApp extends StatelessWidget {
               decoration: BoxDecoration(
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
+                    color: Colors.black.withValues(alpha: 0.1),
                     spreadRadius: 2,
                     blurRadius: 5,
                   ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import app_settings
 import device_info_plus
 import flutter_local_notifications
 import location
@@ -13,6 +14,7 @@ import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
