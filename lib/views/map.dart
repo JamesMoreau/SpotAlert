75c9df7608c121011aa862437072a47a26be4511
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2026-01-20 19:16:24 -0500

    update string

--- lib/views/map.dart
+++ lib/views/map.dart
@@ -479,10 +479,7 @@ void showInfoDialog(BuildContext context) {
               const SizedBox(height: 15),
               const Text('Staying on the map view for long periods of time may drain your battery.', textAlign: .center),
               const SizedBox(height: 15),
-              const Text(
-                'Set location permissions to "While Using" or "Always" and enable notifications to use the app when running in background. Also make sure to disable Silent mode.',
-                textAlign: .center,
-              ),
+              const Text('Set location permissions to "While Using" or "Always" to use the app when running in background.', textAlign: .center),
               TextButton(onPressed: () => Navigator.pop(context), child: const Text('Close')),
             ],
           ),
