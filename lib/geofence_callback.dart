160876d8b004a4f304446ddcd751c2b38541fce3
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2026-01-28 16:19:03 -0500

    cleanup

--- lib/geofence_callback.dart
+++ lib/geofence_callback.dart
@@ -55,19 +55,3 @@ Future<void> geofenceTriggered(GeofenceCallbackParams params) async {
 
   await Future<void>.delayed(const Duration(seconds: 1));
 }
-
-  // 
-  // debugPrint('Alarm with id $id triggered.');
-
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await alarm_package.Alarm.init();
-
-  // Setup and fire the alarm package to bring the user's attention.
-  // var alarmSettings = alarm_package.AlarmSettings(
-  //   id: params.geofences.first.hashCode,
-  //   dateTime: .now(),
-  //   assetAudioPath: 'assets/slow_spring_board_repeated.wav',
-  //   volumeSettings: const .fixed(volume: 0.8, volumeEnforced: true),
-  //   notificationSettings: const .new(title: 'Alarm Triggered', body: 'You have entered the radius of an alarm', stopButton: 'Stop'),
-  // );
-  // await alarm_package.Alarm.set(alarmSettings: alarmSettings);
