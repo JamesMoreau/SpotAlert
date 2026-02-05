9689398d7cbdd14985a7abecba1282545bf629f8
James Moreau <43487194+JamesMoreau@users.noreply.github.com>
2026-02-04 21:27:45 -0500

    added scalebar to map

--- lib/views/map.dart
+++ lib/views/map.dart
@@ -42,6 +42,7 @@ class MapView extends StatelessWidget {
           ),
           children: [
             TileLayer(urlTemplate: openStreetMapTemplateUrl, userAgentPackageName: spotAlert.packageInfo.packageName, tileProvider: spotAlert.tileProvider),
+            const Scalebar(alignment: .bottomLeft, padding: .only(left: 20, bottom: 150)),
             AlarmMarkers(alarms: spotAlert.alarms, threshold: circleToMarkerZoomThreshold),
             UserPosition(stream: spotAlert.positionStream),
             AlarmPlacementMarker(isPlacing: spotAlert.isPlacingAlarm, radius: spotAlert.alarmPlacementRadius),
